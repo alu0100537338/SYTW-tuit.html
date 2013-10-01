e2ce29fb7c1d408dbcf71c1acc06a4c316df4705
Jessica Ramos <audiken_92@hotmail.com>
2013-10-01 20:53:41 +0100

    Probando

--- configure.rb
+++
@@ -1,6 +0,0 @@
-Twitter.configure do |config|
-  config.consumer_key = 'bANPB5Z4Km5M9CVYu7aFbg'
-  config.consumer_secret = 'truwMFNXdIVoHaMlZ9AJLH4ALntoKY8bhNrRnfu04'
-  config.oauth_token = '18429695-uP4xinGtzJ4WvS8ogyPyVfuUdckiToNsrWZctJXqp'
-  config.oauth_token_secret = 'kIGPlEju2CPYA1owzyyofawfY8y1Vy92Qj2YGag0FE'
-end
