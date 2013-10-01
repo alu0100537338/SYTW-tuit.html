aed5bb8351c5b8af9ab67e61711c00c1f9f8207e
Jessica Ramos <audiken_92@hotmail.com>
2013-10-01 20:07:41 +0100

    Casi terminada

--- tuit_html.rb
+++ tuit_html.rb
@@ -1,41 +1,55 @@
 # -*- coding: utf-8 -*-
 require 'twitter'
-require './configure.rb'
+require './configure'
 require 'rack'
 require 'pry-debugger'
 require 'thin'
 require 'erb'
 
-class HelloWorld
+class Twitts
 
-  def call env
-    req = Rack::Request.new(env)
-    res = Rack::Response.new 
-    binding.pry if ARGV[0]
-    res['Content-Type'] = 'text/html'
-    name = (req["firstname"] && req["firstname"] != '') ? req["firstname"] :'World'
-    res.write <<-"EOS"
-      <!DOCTYPE HTML>
-      <html>
-        <title>Rack::Response</title>
-        <body>
-          <h1>
-             Hello #{name}!
-             <form action="/" method="post">
-               Your name: <input type="text" name="firstname" autofocus><br>
-               <input type="submit" value="Submit">
-             </form>
-          </h1>
-        </body>
-      </html>
-    EOS
-    res.finish
-  end
+	#Inicializar variables
+	def initialize
+		@tuits = []
+		@nombre = ''
+		@numero = 0		
+	end
+
+	#Acceso al HTML para mostrar los resultados
+	def erb(template)
+  		template_file = File.open("tuit.html.erb", 'r')
+  		ERB.new(File.read(template_file)).result(binding)
+	end
+	
+	#Método call
+	def call env
+	    req = Rack::Request.new(env)
+	    
+	    binding.pry if ARGV[0]
+	   
+	   #Si no esta vacio , no es un espacio y el usuario existe en Twitter el nombre es el introducido
+	    #@name = (req["firstname"] && req["firstname"] != '' && Twitter.user?(req["firstname"]) == true ) ? req["firstname"] : ''
+
+		#@number = (req["n"] && req["n"].to_i>1 ) ? req["n"].to_i : 1
+		#puts "#{req["n"]}"
+		
+		#Si el nombre existe buscamos sus últimos Tweets
+		#if @nombre == req["firstname"]
+			puts "#{@tuits}"
+			ultimos = Twitter.user_timeline(@nombre,{:count=>@numero.to_i})
+			@tuits =(@tuits && @tuits != '') ? ultimos.map{ |i| i.text} : ''				
+		#end
+
+		#Invoca a erb
+		Rack::Response.new(erb('tuit.html.erb'))
+	end
+
 end
 
-Rack::Server.start(
-  :app => HelloWorld.new,
-  :Port => 9292,
-  :server => 'thin'
-)
-
+if $0 == __FILE__
+	Rack::Server.start( 
+		:app => Twitts.new,
+	    	:Port => 9292,
+	    	:server => 'thin'
+  	)
+end
